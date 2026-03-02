e74cdbe7feca993291e3dcf4a1da2ee726070462
Soumya Ranjan Acharya <YOUR_GITHUB_EMAIL>
2026-03-02 16:33:12 +0530

    Added PlotingGraph file

--- CreateAndReadFile.py
+++
@@ -1,9 +0,0 @@
-with open("sonu.txt", "w") as file:
-    file.write("Hello, this is a sample text file.\n")
-
-with open("sonu.txt", "r") as file:
-    print(file.read())
-
-#r+ is used to read and write the file
-# a+ is used to append and read the file
-# w+ is used to write and read the file
--- Password_craker.py
+++
@@ -1,4 +0,0 @@
-import random
-x=[random.randint(0,100)
-print("The password is: ",x)
-print()
